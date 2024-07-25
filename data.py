0f4cab99fecf0933dd6378f509053957067bc626
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2024-07-25 09:23:36 +0330

    Update data.py

--- data.py
+++ data.py
@@ -3,10 +3,12 @@ import bcrypt
 from datetime import datetime
 from dotenv import load_dotenv
 from sqlmodel import Field, SQLModel, create_engine, Session, select
-from model import User,RegisterModel
+from model import User, RegisterModel
 
+# Load environment variables
 load_dotenv()
 
+# Verify if environment variables are loaded
 DATABASE_HOST = os.getenv("DATABASE_HOST")
 DATABASE_USER = os.getenv("DATABASE_USER")
 DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
@@ -15,10 +17,13 @@ DATABASE_URL = (
     f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
 )
 
-engine = create_engine(DATABASE_URL, echo=True)
+print("DATABASE_HOST:", DATABASE_HOST)
+print("DATABASE_USER:", DATABASE_USER)
+print("DATABASE_PASSWORD:", DATABASE_PASSWORD)
+print("DATABASE_NAME:", DATABASE_NAME)
+print("DATABASE_URL:", DATABASE_URL)
 
-# DATABASE_URL = 'sqlite:///./database.db'
-
+# Create database engine
 engine = create_engine(DATABASE_URL, echo=True)
 
 # Create the database tables
@@ -30,26 +35,25 @@ def get_user_by_username(username: str):
         return db_session.exec(statement).first()
     
 def create_user(user_data: RegisterModel):
-        password_bytes = user_data.password.encode('utf-8')
-        password_hash = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
-        user = User(
-            first_name=user_data.first_name,
-            last_name=user_data.last_name,
-            email=user_data.email,
-            username=user_data.username,
-            age=user_data.age,
-            city=user_data.city,
-            country=user_data.country,
-            jon_time = datetime.datetime.now(),
-            password_hash=password_hash
-        )
-        with Session(engine) as db_session:
-            db_session.add(user)
-            db_session.commit()
-            db_session.refresh(user)
-        return user
-
+    password_bytes = user_data.password.encode('utf-8')
+    password_hash = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+    user = User(
+        first_name=user_data.first_name,
+        last_name=user_data.last_name,
+        email=user_data.email,
+        username=user_data.username,
+        age=user_data.age,
+        city=user_data.city,
+        country=user_data.country,
+        jon_time=datetime.now(),  # Corrected datetime.datetime.now() to datetime.now()
+        password_hash=password_hash
+    )
+    with Session(engine) as db_session:
+        db_session.add(user)
+        db_session.commit()
+        db_session.refresh(user)
+    return user
 
 def verify_password(password: str, password_hash: str) -> bool:
-        password_bytes = password.encode('utf-8')
-        return bcrypt.checkpw(password_bytes, password_hash)
+    password_bytes = password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, password_hash)
