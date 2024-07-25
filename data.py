cce00bc8fa675a8fa4df3b8bf8e5f4dceef372c9
Mohamad Nematizadeh <mohammad.nematizzadeh@gmail.com>
2024-07-25 10:47:39 +0330

    Update data.py

--- data.py
+++ data.py
@@ -25,25 +25,27 @@ def get_user_by_username(username: str):
         return db_session.exec(statement).first()
     
 def create_user(user_data: RegisterModel):
-    password_bytes = user_data.password.encode('utf-8')
-    password_hash = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
-    user = User(
-        first_name=user_data.first_name,
-        last_name=user_data.last_name,
-        email=user_data.email,
-        username=user_data.username,
-        age=user_data.age,
-        city=user_data.city,
-        country=user_data.country,
-        jon_time=datetime.now(),  # Corrected datetime.datetime.now() to datetime.now()
-        password_hash=password_hash
-    )
-    with Session(engine) as db_session:
-        db_session.add(user)
-        db_session.commit()
-        db_session.refresh(user)
-    return user
+        password_bytes = user_data.password.encode('utf-8')
+        password_hash = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+        user = User(
+            first_name=user_data.first_name,
+            last_name=user_data.last_name,
+            email=user_data.email,
+            username=user_data.username,
+            age=user_data.age,
+            city=user_data.city,
+            country=user_data.country,
+            jon_time = datetime.datetime.now(),
+            password_hash=password_hash
+        )
+        with Session(engine) as db_session:
+            db_session.add(user)
+            db_session.commit()
+            db_session.refresh(user)
+        return user
+
 
 def verify_password(password: str, password_hash: str) -> bool:
-    password_bytes = password.encode('utf-8')
-    return bcrypt.checkpw(password_bytes, password_hash)
+        password_bytes = password.encode('utf-8')
+        return bcrypt.checkpw(password_bytes, password_hash)
+
